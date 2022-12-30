d7c7f64ae5c1c35b035aadec315ddd237757bb4f
kroner <akronerr@gmail.com>
2022-12-30 17:47:35 +0400

    moved css and js
    now it looks pretty good

---
+++ script.js
@@ -0,0 +1,3 @@
+function showHelloWorld() {
+    open("https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/d24dc6c7bdf9d3c426f137780fec0d54~c5_720x720.jpeg?x-expires=1672128000&x-signature=RaixQfjzytDX%2F22rvD%2FRstbCLqE%3D")
+}
