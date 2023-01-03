18dec6cb3d2f0841d7b229bb382a745ea6add747
kroner <akronerr@gmail.com>
2023-01-03 10:37:04 +0400

    changes in text

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function showHelloWorld() {
     open("https://i.kym-cdn.com/entries/icons/original/000/037/848/cover2.jpg")
+
+    // fetch("https://www.instagram.com/api/v1/users/web_profile_info/?username=kroner_abd", {
+    //     "headers": {
+    //         "accept": "*/*",
+    //         "accept-language": "ru-RU,ru;q=0.9",
+    //         "sec-ch-prefers-color-scheme": "dark",
+    //         "sec-ch-ua": "\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Google Chrome\";v=\"108\"",
+    //         "sec-ch-ua-mobile": "?0",
+    //         "sec-ch-ua-platform": "\"Windows\"",
+    //         "sec-fetch-dest": "empty",
+    //         "sec-fetch-mode": "cors",
+    //         "sec-fetch-site": "same-origin",
+    //         "viewport-width": "876",
+    //         "x-asbd-id": "198387",
+    //         "x-csrftoken": "fgRtocUvcS7rKGO82i0J6AQpcP8BIlp5",
+    //         "x-ig-app-id": "936619743392459",
+    //         "x-ig-www-claim": "0",
+    //         "x-requested-with": "XMLHttpRequest",
+    //         "Access-Control-Allow-Origin": "*"
+    //     },
+    //     "body": null,
+    //     "method": "GET",
+    //     "mode": "cors",
+    //     "credentials": "include"
+    // }).then((response) => console.log(response));
 }
